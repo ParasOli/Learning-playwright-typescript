020de3883081c13751f0c999a9194d26e72a9258
ParasOli <parasoli7379@gmail.com>
2026-06-09 23:45:28 +0545

    my first two test cases

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+
+test('login with valid credentials should redirect to inventory page', async({page})=>{
+  await page.goto('https://www.saucedemo.com/')
+  await page.locator('#user-name').fill('standard_user')
+  await page.locator('#password').fill('secret_sauce')
+  await page.locator('[data-test="login-button"]').click()
+  await expect(page).toHaveURL(new RegExp('inventory.html'));
+  await expect(page.locator('.app_logo')).toContainText('Swag Labs');
+await expect(page.locator('.app_logo')).toBeVisible();
+})
 
 
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-
+test('login with invalid password should show error message',async ({page})=>{
+  await page.goto('https://www.saucedemo.com/')
+  await page.locator('#user-name').fill('standard_user')
+  await page.locator('#password').fill('random password')
+   await page.locator('[data-test="login-button"]').click()
+  await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service')
+  await expect(page).toHaveURL(new RegExp('https://www.saucedemo.com/'))
+})
