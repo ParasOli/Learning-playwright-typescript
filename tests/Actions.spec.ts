f47c27f9a68eaf5f5689ef604dcb0aff27c78b55
ParasOli <parasoli7379@gmail.com>
2026-06-11 02:05:48 +0545

    input,radio button, checkbox practice

--- tests/Actions.spec.ts
+++ tests/Actions.spec.ts
@@ -1,7 +1,7 @@
 
 import {test, expect, Locator} from '@playwright/test'
 
-test('example', async ({page })=>{
+test('input', async ({page })=>{
     await page.goto('https://testautomationpractice.blogspot.com/')
     const nameInput: Locator = page.locator('#name')
     await expect(nameInput).toBeVisible();
@@ -14,7 +14,28 @@ test('example', async ({page })=>{
     const inputValue: string = await (nameInput).inputValue()
     console.log(inputValue)
     expect(inputValue).toBe('paras')
+})
 
-    
 
+    test('radio button', async ({page})=>{
+        await page.goto('https://testautomationpractice.blogspot.com/')
+        const maleRadioButton:Locator = page.locator('#male')
+        expect(maleRadioButton).toBeVisible()
+        await (maleRadioButton).check()
+        expect(maleRadioButton).toBeChecked()
+        const conditionOfCheckBox = await(maleRadioButton.isChecked())
+        expect(conditionOfCheckBox).toBe(true)
+})
+
+test('check box', async ({page})=>{
+     await page.goto('https://testautomationpractice.blogspot.com/')
+     const day:string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+    for(const d of day){
+     const checkBox:Locator = page.getByLabel(d)
+     await checkBox.check()
+     expect(checkBox).toBeChecked()
+     const ConditionOfCheck = await(checkBox.isChecked())
+     expect(ConditionOfCheck).toBe(true)
+    }
 })
