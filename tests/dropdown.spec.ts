c1caddba088ee6fb5b7b6144bb9912783cc2d1bd
ParasOli <parasoli7379@gmail.com>
2026-06-15 00:15:30 +0545

    test case added fo dropdown multiple

--- tests/dropdown.spec.ts
+++ tests/dropdown.spec.ts
@@ -15,8 +15,37 @@ test('Single select DropDown', async({page})=>{
       
 })
 
-test.only('Multiple Select DropDown', async ({page})=>{
+test('Multiple Select DropDown', async ({page})=>{
     await page.goto('https://testautomationpractice.blogspot.com/')
+    const colorOptions:Locator = await page.locator('#animals option')
+    const lenghtofArray = await colorOptions.count()
+    const arrayOfColors:string[] = (await colorOptions.allTextContents()).map(text=>text.trim())
+    const sortedArrayOfColors:string[] = [...arrayOfColors].sort()
+    expect (sortedArrayOfColors).toEqual(arrayOfColors)
+
+})
+
+test.only('Validate the drop down dont have any duplicate options', async ({page})=>{
+        await page.goto('https://testautomationpractice.blogspot.com/')
+        const colorOptions:Locator = await page.locator('#animals option')
+        const colorArray:string[] = (await colorOptions.allTextContents()).map(text=>text.trim())
+        const newArray:string[] = []
+
+        const mySet = new Set<string>()
+
+        for(const color of colorArray){
+            if(mySet.has(color)){
+                newArray.push(color)
+            }else{
+                mySet.add(color)
+            }
+        }
+        console.log(mySet)
+        console.log(newArray)
+
+        expect(newArray).toHaveLength(0)
+
+
 
 })
 
