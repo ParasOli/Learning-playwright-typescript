bee17af0fd18f00a9abf1d057f156bdd2e37e73c
ParasOli <parasoli7379@gmail.com>
2026-06-17 01:15:00 +0545

    hidden Bootstrap dropdown test added

---
+++ tests/hiddenBootstrapDropdown.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect, Locator } from '@playwright/test'
+
+test('validating the dropdown', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/')
+
+  const nameField: Locator = page.locator('[name="username"]')
+  await nameField.fill('admin')
+
+  const passwordField: Locator = page.locator('[name="password"]')
+  await passwordField.fill('admin123')
+
+  await page.locator('button[type="submit"]').click()
+  await page.locator('[href="/web/index.php/pim/viewPimModule"]').click()
+
+  const label = page.locator('label').filter({ hasText: 'Employment Status' })
+  await expect(label).toBeVisible()
+
+  await label.locator('..').locator('..').locator('i').first().click()
+
+  await page.waitForTimeout(3000)
+
+  const options: Locator = page.locator('[role="option"] span')
+  console.log(await options.count())
+
+  const arrayOfOptions: string[] = (await options.allTextContents()).map((text) =>
+    text.trim()
+  )
+
+  for (const option of arrayOfOptions) {
+    if (option.toLowerCase() === 'freelance') {
+      await page.locator('[role="option"] span').filter({ hasText: 'Freelance' }).click()
+    }
+  }
+
+  await expect(
+    label.locator('..').locator('..').locator('.oxd-select-text-input')
+  ).toHaveText('Freelance')
+})
