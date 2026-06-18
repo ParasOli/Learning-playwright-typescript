54d45ebdd692053f068696d82c1530fe5e70d481
ParasOli <parasoli7379@gmail.com>
2026-06-19 00:09:12 +0545

    get the data of the second row test case

--- tests/staticTable.spec.ts
+++ tests/staticTable.spec.ts
@@ -10,15 +10,22 @@
  })
 
 
-test.only('Validating the table column count match', async ({ page }) => {
+test('Validating the table column count match', async ({ page }) => {
   await page.goto('https://testautomationpractice.blogspot.com/')
-
   const columnLocator: Locator = page.locator('table[name="BookTable"] th')
   const columnCount = await columnLocator.count()
-
   console.log(columnCount)
-
   expect(columnCount).toBe(4)
+})
+
+test.only('get the data of the second row', async ({page})=>{
+      await page.goto('https://testautomationpractice.blogspot.com/')
+        const rowsLocator:Locator = page.locator('table[name="BookTable"] tr')
+      const data: string[] = await rowsLocator.nth(2).locator('td').allInnerTexts()
+  .then(data => data.map(text => text.trim()));
+  console.log(data)
+
+
 })
 
 
