2918918e0e1ae60db1db3037c2e7dccb84babc37
ParasOli <parasoli7379@gmail.com>
2026-06-18 23:54:29 +0545

    table header count test

--- tests/staticTable.spec.ts
+++ tests/staticTable.spec.ts
@@ -1,11 +1,25 @@
  import {Locator,expect, test} from "@playwright/test"
 
- test.only('Validating the table count match', async ({page})=>{
+ test('Validating the table row count match', async ({page})=>{
   await page.goto('https://testautomationpractice.blogspot.com/')
   const rowsLocator:Locator = page.locator('table[name="BookTable"] tr')
   const rowsCount = await rowsLocator.count()
   console.log(rowsCount)
+  //including header
   expect(rowsCount).toBe(7)
-
-      
  })
+
+
+test.only('Validating the table column count match', async ({ page }) => {
+  await page.goto('https://testautomationpractice.blogspot.com/')
+
+  const columnLocator: Locator = page.locator('table[name="BookTable"] th')
+  const columnCount = await columnLocator.count()
+
+  console.log(columnCount)
+
+  expect(columnCount).toBe(4)
+})
+
+
+
