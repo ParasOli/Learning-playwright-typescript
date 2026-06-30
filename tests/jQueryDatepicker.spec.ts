3b0a72ec133ea141c1b8d7f1d906906d356faaed
ParasOli <parasoli7379@gmail.com>
2026-06-30 23:33:06 +0545

    JQuery datepicker for future date - using loop--- test added

--- tests/jQueryDatepicker.spec.ts
+++ tests/jQueryDatepicker.spec.ts
@@ -7,3 +7,38 @@ test('JQuery datepicker- using fill', async ({ page }) => {
     await page.waitForTimeout(4000) 
 
 })
+
+test.only('JQuery datepicker for future date - using loop', async ({ page }) => {
+    await page.goto('https://testautomationpractice.blogspot.com/');
+    const datePicker:Locator = page.locator('#datepicker')
+    await datePicker.click()
+
+
+    const year ='2027'
+    const month = 'August'
+    const day = '16'
+
+    while(true){
+
+        const currentMonth = await page.locator('.ui-datepicker-month').innerText()
+        const currentyear = await page.locator('.ui-datepicker-year').innerText()
+
+        if(currentMonth===month && currentyear===year){
+            break;
+        }
+        await page.locator('[data-handler="next"]').click()
+
+    }
+
+    const dateLocator= await page.locator('.ui-datepicker-calendar td').all()
+
+    for(let dt of dateLocator){
+        const dateText = await dt.innerText()
+        if(dateText===day){
+            await dt.click()
+            break;
+        }
+    }
+await expect(datePicker).toHaveValue(new RegExp(year))
+
+})
