7840e3a328e001e776cda5e056cce2f12000b8be
ParasOli <parasoli7379@gmail.com>
2026-07-03 00:28:31 +0545

    fix in the pop up tests

--- tests/popups.spec.ts
+++ tests/popups.spec.ts
@@ -1,18 +1,19 @@
 import {expect, test} from "@playwright/test"
 
-test('popups', async ({browser})=>{
+test('should open selenium.dev in a new tab, verify its heading, and close it', async ({browser})=>{
     const context = await browser.newContext()
     const page = await context.newPage()
 
  await page.goto('https://testautomationpractice.blogspot.com/')
 
  const [childPage] = await Promise.all([context.waitForEvent('page'), await page.locator('button#PopUp').click()])
- const pageTitle = childPage.title()
+ const pageTitle = await childPage.title()
  console.log(pageTitle)
-const url = childPage.url()
-console.log(url)
-// expect(childPage.url).toStrictEqual('https://www.selenium.dev/')
-// expect(page.locator('h4 .alert-heading')).toContainText('Thank you for joining the Selenium and Appium 2026 Conference.')
+expect(childPage.url()).toStrictEqual('https://www.selenium.dev/')
+
+await expect(childPage.locator('h4.alert-heading')).toContainText('Thank you for joining the Selenium and Appium 2026 Conference.')
+await childPage.close()
+
 
 
 })
