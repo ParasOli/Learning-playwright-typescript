71ca0759dcda899fb699f030ceccd8589f05682d
ParasOli <parasoli7379@gmail.com>
2026-07-09 00:07:42 +0545

    multiple file upload test added

--- tests/uploadFiles.spec.ts
+++ tests/uploadFiles.spec.ts
@@ -2,9 +2,12 @@ import {expect, Locator, test} from "@playwright/test"
 
 const SELECTORS = {
     fileInput: '#singleFileInput',
+    multipleFileInput: '#multipleFilesInput',
     form: '#singleFileForm',
+    multipleFileInputForm: '#multipleFilesForm',
     submitBtn: '[type="submit"]',
-    statusMessage: '#singleFileStatus'
+    statusMessage: '#singleFileStatus',
+    multipleFilesStatusMessage:'#multipleFilesStatus'
 }
 
 const EXPECTED_MESSAGE = 'Single file selected'
@@ -23,4 +26,17 @@ test('should upload a single file and display success message', async ({page})=>
     await statusMessage.waitFor({state: 'visible'})
     const message = await statusMessage.innerText()
     expect(message).toContain(EXPECTED_MESSAGE)
+})
+
+test('should upload multiple file and display success message', async ({page})=>{
+        await page.goto('https://testautomationpractice.blogspot.com/')
+        await page.waitForLoadState('networkidle')
+        const fileSelector = page.locator(SELECTORS.multipleFileInput)
+        await fileSelector.setInputFiles(['fixtures/demo.pdf', 'fixtures/demoSecond.pdf'])
+        await page.locator(SELECTORS.multipleFileInputForm).locator(SELECTORS.submitBtn).click()
+        const message = await page.locator(SELECTORS.multipleFilesStatusMessage).innerText()
+        console.log(message)
+        expect(message).toContain('demo.pdf')
+        expect(message).toContain('demoSecond.pdf')
+
 })
